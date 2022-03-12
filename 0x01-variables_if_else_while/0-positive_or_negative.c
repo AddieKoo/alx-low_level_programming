6da2fae5ffc95c5fa741e22004742c2121bd51f2
AddieKoo <addo.adofowaah@gmail.com>
2022-03-11 17:02:35 -0800

    randoms

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <time.h>
 int main()
 {
-int rand_num;
-srand(time(0));
-printf("The randomly generated number is");
-rand_num = rand();
-printf("%d\n", rand_num);
+srand(time(NULL));
+printf("%d\n", rand());
+printf("%d\n", rand());
+printf("%d\n", rand());
+printf("%d\n", rand());
 }
