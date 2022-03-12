5de52cc987afc8535bf118b6f64c0c8d704b754a
AddieKoo <addo.adofowaah@gmail.com>
2022-03-12 05:21:09 -0800

    prints alphabets in lowercase plus new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,11 @@
 int main(void)
 {
 int ch;
-for (ch != 'a') && (ch != 'q'))
-putchar(ch);
-putchar('\n');
+for (ch = 'a'; ch <= 'z'; ch++)
+if ((ch != 'e') && (ch != 'q'))
+{
+	putchar(ch);
+}
+	putchar('\n');
 return (0);
 }
