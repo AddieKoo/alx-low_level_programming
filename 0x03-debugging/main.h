d330edd79c3e789f29eef38b5e0325713cbe5aaa
Akosua <addo.adofowaah@gmail.com>
2022-03-18 00:11:28 +0000

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,19 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-{
-	if( i < 0)
-	{
-		printf("i is negative\n");
-	}
-	else if( i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("i is positive\n");
-	}
-}
-#endif /* MAIN_H */
 
+
+#endif /* MAIN_H */
