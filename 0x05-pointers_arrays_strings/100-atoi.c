425f6990faedc45bd313f989f1472c29d6ca58eb
Akosua <addo.adofowaah@gmail.com>
2022-03-22 02:46:40 +0000

    converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
+ * is_numerical - check if it is a digit
+ * @n: number
+ * Return: if it is a number, return 1 else, return 0
+ */
+int is_numerical(unsigned int n)
+{
+return (n >= '0' && n <= '9');
+}
+/**
  * _atoi - converts a string to an integer
  * @s: string
  *
