c3116cce93bc968befedaae57f4a6aa44cb3480b
Akosua <addo.adofowaah@gmail.com>
2022-03-29 05:32:57 +0000

    sets value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets value of a pointer to a char
+ * @s: a pointer to be checked
+ * @to: points to a char
+ *
+ * Return: char
+ */
+void set_string(char **s, char *to)
+{
+
