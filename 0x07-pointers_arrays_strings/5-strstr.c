ac6637ebb309bfa05d3320f98370b047267fe116
Akosua <addo.adofowaah@gmail.com>
2022-03-29 04:54:14 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#inclde "stddef.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string to be checked
+ * @needle: string to be checked
+ *
+ * Return: a pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, flag = 0;
+if (*needle == '\0')
+{
+return (haystack);
+}
+for (; haystack[i] != '\0'; i++)
+{
+int j = 0;
+if (haystack[i] == needle[0])
+{
+if (haystack[i + j] != needle[j])
+{
+flag = 0;
+break;
+}
+else
+flag = 1;
+j++;
+}
+if (flag)
+break;
+}
+}
+if (flag)
+return (haystack + i);
+else
+return (NULL);
+}
