03793decc8a8fbfa5e719c778eca6f364f331228
Akosua <addo.adofowaah@gmail.com>
2022-03-30 18:21:49 +0000

    returns 1 if string is palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_palindrome - returns 1 if a string is palindrome, else 0
+ * @s: string to be checked
+ *
+ * Return: 1 if s is a palindrome, else 0
+ */
+int is_palindrome(char *s)
+{
+int flag = 1;
+check(s, 0, _strlen_recursion(s) - 1, &flag);
+return (flag);
+}
