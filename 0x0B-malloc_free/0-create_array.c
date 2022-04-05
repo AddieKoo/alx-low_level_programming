e0bce97e812a7180c3303f1bac4725439dd62946
Akosua <addo.adofowaah@gmail.com>
2022-04-05 00:54:32 +0000

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * create_array - creates an array of chars and initializes it 
+ * create_array - creates an array of chars and initializes it
  * @size: size of the array
  * @c: character to insert
  * Return: NULL if fails, pointer to array if success
@@ -14,7 +14,7 @@ return (NULL);
 array = malloc(sizeof(char) * size);
 if (array == NULL)
 return (NULL);
-for (index = 0; index < size; index ++)
+for (index = 0; index < size; index++)
 array[index] = c;
 return (array);
 }
