f7fdf6eac39724cc5916314a054ed9923a2ba840
Akosua <addo.adofowaah@gmail.com>
2022-04-06 01:45:38 +0000

    static  library

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,1 +1,27 @@
-char *_strstr(char *haystack, char *needle);
+#include "main.h?"
+/**
+ * _strstr - checks for substring needle in string haystack
+ * @haystack: string where search is made
+ * @needle: string that is checked for
+ * Return: pointer to start of found substring or else NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+i = 0;
+if (haystack[i] == needle[i])
+{
+do {
+if (needle[i + 1] == '\0')
+return (haystack);
+i++;
+} while (haystack[i] == needle[i]);
+}
+haystack++;
+}
+return ('\0');
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,15 @@
-int _abs(int n);
+#include "main.h"
+/**
+ * _abs - prints the absolute value of a number
+ * @n: number targeted
+ * Return: 0
+ */
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else if (n < 0)
+return (-n);
+else
+return (0);
+}
