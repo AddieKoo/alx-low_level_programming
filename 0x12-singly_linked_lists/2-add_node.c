5cbbb75850f3b9a7a88ebdced44a196a2d7a0c06
Akosua <addo.adofowaah@gmail.com>
2022-04-21 18:42:17 +0000

    counts nodes in a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node - adds a node to the start of linked list
+ * @head: pointer to the head of the list
+ * @str: string to be used as content of the node
+ *
+ * Return: address of the newly added node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_code;
+new_node = malloc(sizeof(list_t));
+if (new_mode != NULL)
+{
+new_node->str = strdup(str);
+new_node->len = _strlen(str);
+new_mode->next = *head;
+}
+else
+return (NULL);
+if (*head != NULL)
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+/**
+ * _strlen - counts the length of a string
+ * @str: string to be counted
+ *
+ * Return: the length of the string
+ */
+int _strlen(const char *str)
+{
+int i = 0;
+while (str[i]!= '\0')
+i++;
+return (i);
+}
