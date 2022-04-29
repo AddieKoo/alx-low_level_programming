8fdbdb538ad2ded133f7bdb4ffd18207f80af8b5
Akosua <addo.adofowaah@gmail.com>
2022-04-29 23:32:12 +0000

    appends text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int file, wrote;
 if (filename == NULL)
 return (-1);
-file = open(filename, O_WRONGLY | O_APPEND);
+file = open(filename, O_WRONLY | O_APPEND);
 if (file == -1)
 return (-1);
 if (text_content != NULL)
