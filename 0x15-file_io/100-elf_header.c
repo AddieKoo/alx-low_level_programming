22e146a635bf192a5e7cf9fc9450a97cd5589832
Akosua <addo.adofowaah@gmail.com>
2022-04-30 00:09:03 +0000

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -202,3 +202,87 @@ default:
 printf("<unknown: %x>\n", e_type);
 }
 }
+/**
+ * print_entry - prints the entry point of an ELF header
+ * @e_entry: the address of the ELF entry point
+ * @e_ident: a pointer to an array containing the ELF class
+ */
+void print_entry(unsigned long int e_entry, unsigned char *e_ident)
+{
+printf("  Entry point address:           ");
+if (e_ident[EI_DATA] == ELFDATA2MSB)
+{
+e_entry = ((e_entry << 8) & 0xFF00FF00) |
+((e_entry >> 8) & 0xFF00FF);
+e_entry = (e_entry << 16) | (e_entry >> 16);
+}
+if (e_ident[EI_CLASS] == ELFCLASS32)
+printf("%#x\n", (unsigned int)e_entry);
+else
+printf("%#]x\n", e_entry);
+}
+/**
+ * close_elf - closes an elf file
+ * @elf: the file descriptor of the ELF file
+ *
+ * Description: if the file cannot be closed, exit(98)
+ */
+void close_elf(int elf)
+{
+if (close(elf) == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't close fd %d\n", elf);
+exit(98);
+}
+}
+/**
+ * main - displays info in ELF header at start of ELF file
+ * @argc: argument count
+ * @argv: an array of pointers ro the arguments
+ *
+ * Return: 0 on success
+ *
+ * Description: if the file is not an ELF file or failed function, 98
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+Elf64_Ehdr *header;
+int o, r;
+o = open(argv[1], O_RDONLY);
+if (o == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+exit(98);
+}
+header = malloc(sizeof(Elf64_Ehdr));
+if (header == NULL)
+{
+close_elf(o);
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+exit(98);
+}
+r = read(o, header, sizeof(Elf64_Ehdr));
+if (r == -1)
+{
+free(header);
+close_elf(o);
+dprintf(STDERR_FILENO, "Error: '%s': No such file\n", argv[1]);
+exit(98);
+}
+
+check_elf(header->e_ident);
+printf("ELF Header:\n");
+print_magic(header->e_ident);
+print_class(header->e_ident);
+print_data(header->e_ident);
+print_version(header->e_ident);
+print_osabi(header->e_ident);
+print_abi(header->e_ident);
+print_type(header->e_type, header->e_ident);
+print_entry(header->e_entry, header->e_ident);
+
+free(header);
+close_elf(o);
+return (0);
+}
